20e7cba54bbe2f280ca4d96bfdf55e1ca5990b6d
3mpty <aurelien.corre@outlook.fr>
2024-03-08 22:55:26 +0100

    mobile homepage done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import Link from "next/link";
+import styles from "./homepage.module.scss";
+import Image from "next/image";
+
+import star from "/public/img/star.svg";
+import profilPicture from "/public/img/profilPic.png";
+
 export default function Home() {
-    return <></>;
+    return (
+        <>
+            <div className={styles.container}>
+                <h1>
+                    CORRE<span>Aurélien</span>
+                </h1>
+                <p>
+                    <span>frontend developer</span> that spends{" "}
+                    <span>more</span> (oops) <span>less</span> time drinking
+                    coffee than coding stuff.
+                </p>
+                <Link className={styles.availableLink} href={"#contact"}>
+                    <p>Available for work</p>
+                    <Image src={star} alt="Star icon" width={20} height={20} />
+                </Link>
+                <figure>
+                    <Image
+                        src={profilPicture}
+                        alt="Current profil picture"
+                        fill
+                    />
+                </figure>
+            </div>
+        </>
+    );
 }
