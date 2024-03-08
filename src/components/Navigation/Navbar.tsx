af868bac09748e234357ef2a818099c1221cbd3c
3mpty <aurelien.corre@outlook.fr>
2024-03-08 22:13:16 +0100

    Desktop navbar done

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -3,6 +3,10 @@
 import React, { useState } from "react";
 import styles from "./navbar.module.scss";
 import Link from "next/link";
+import useDimension from "@/hooks/useDimensions";
+import Image from "next/image";
+
+import star from "/public/img/star.svg";
 
 type NavLinks = {
     name: string;
@@ -10,7 +14,6 @@ type NavLinks = {
 };
 
 const Navlinks: NavLinks[] = [
-    { name: "Home", path: "/" },
     { name: "Projetcs", path: "#gallery" },
     { name: "About", path: "#about" },
     { name: "Contact", path: "#contact" },
@@ -18,6 +21,7 @@ const Navlinks: NavLinks[] = [
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const { width } = useDimension();
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -29,45 +33,67 @@ const Navbar = () => {
                 <h1>
                     CORRE<span>aurélien</span>
                 </h1>
-                <div onClick={toggleMenu} className={styles.hamburgerContainer}>
-                    <div
-                        style={
-                            isOpen
-                                ? {
-                                      transform: "rotate(135deg)",
-                                      position: "absolute",
-                                  }
-                                : {}
-                        }
-                        className={styles.line}></div>
-                    <div
-                        style={{ opacity: isOpen ? "0" : "1" }}
-                        className={styles.line}></div>
-                    <div
-                        style={
-                            isOpen
-                                ? {
-                                      transform: "rotate(-135deg)",
-                                      position: "absolute",
-                                  }
-                                : {}
-                        }
-                        className={styles.line}></div>
-                </div>
-                <div
-                    style={{ left: isOpen ? "0" : "-100%" }}
-                    className={styles.menu}>
-                    <ul>
-                        {Navlinks.map((link, index) => (
-                            <Link
-                                onClick={toggleMenu}
-                                href={link.path}
-                                key={index}>
-                                <li>{link.name}</li>
-                            </Link>
-                        ))}
-                    </ul>
-                </div>
+                {width < 800 ? (
+                    <>
+                        <div
+                            onClick={toggleMenu}
+                            className={styles.hamburgerContainer}>
+                            <div
+                                style={
+                                    isOpen
+                                        ? {
+                                              transform: "rotate(135deg)",
+                                              position: "absolute",
+                                          }
+                                        : {}
+                                }
+                                className={styles.line}></div>
+                            <div
+                                style={{ opacity: isOpen ? "0" : "1" }}
+                                className={styles.line}></div>
+                            <div
+                                style={
+                                    isOpen
+                                        ? {
+                                              transform: "rotate(-135deg)",
+                                              position: "absolute",
+                                          }
+                                        : {}
+                                }
+                                className={styles.line}></div>
+                        </div>
+                        <div
+                            style={{ left: isOpen ? "0" : "-100%" }}
+                            className={styles.menu}>
+                            <ul>
+                                {Navlinks.map((link, index) => (
+                                    <Link
+                                        onClick={toggleMenu}
+                                        href={link.path}
+                                        key={index}>
+                                        <li>{link.name}</li>
+                                    </Link>
+                                ))}
+                            </ul>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <ul className={styles.desktopLinks}>
+                            {Navlinks.map((link, index) => (
+                                <Link href={link.path} key={index}>
+                                    <li>{link.name}</li>
+                                </Link>
+                            ))}
+                        </ul>
+                        <Link
+                            className={styles.availableLink}
+                            href={"#contact"}>
+                            <p>Available for work</p>
+                            <Image src={star} alt="Star icon" />
+                        </Link>
+                    </>
+                )}
             </nav>
         </>
     );
