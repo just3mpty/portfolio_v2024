bff14dd72db350c28c1963d97cf0e3f185936865
3mpty <aurelien.corre@outlook.fr>
2024-03-10 00:10:42 +0100

    Added links to navigate between projects

--- src/app/[projectId]/page.tsx
+++ src/app/[projectId]/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import data from "../../../public/data.json";
 import styles from "./selectedProject.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+
+import externalLink from "../../../public/icons/link.svg";
+import arrow from "../../../public/icons/arrow.svg";
 
 const page = ({ params }: { params: { projectId: string } }) => {
     const { projectId } = params;
@@ -11,6 +16,76 @@ const page = ({ params }: { params: { projectId: string } }) => {
     return (
         <section className={styles.container}>
             <h1>{selectedProject?.title}</h1>
+            <div className={styles.headingContent}>
+                <video
+                    src={selectedProject?.video}
+                    autoPlay
+                    muted
+                    loop
+                    width={200}
+                />
+                <div className={styles.infos}>
+                    <h3>Description</h3>
+                    <p>{selectedProject?.description}</p>
+                    <h4>Tech stack</h4>
+                    <div className={styles.techStack}>
+                        {selectedProject?.techStack.map((tech, index) => (
+                            <Image
+                                src={tech}
+                                key={index}
+                                alt="Tech icon"
+                                width={60}
+                                height={60}
+                            />
+                        ))}
+                    </div>
+                    <Link
+                        href={`${selectedProject?.linkToWebiste}`}
+                        target="_blank">
+                        <p>See live</p>
+                        <Image
+                            src={externalLink}
+                            alt="External link icon"
+                            width={16}
+                            height={16}
+                        />
+                    </Link>
+                </div>
+            </div>
+            <div className={styles.screenshots}>
+                {selectedProject?.images.map((image, index) => (
+                    <figure key={index}>
+                        <Image
+                            src={image}
+                            alt={`${selectedProject.title}'s screenshots`}
+                            width={2000}
+                            height={2000}
+                        />
+                    </figure>
+                ))}
+            </div>
+            <div className={styles.navigation}>
+                <Link href={"/"} target="#gallery">
+                    <Image
+                        src={arrow}
+                        alt="Arrow icon"
+                        width={50}
+                        height={25}
+                    />
+                    <p>Back to homepage</p>
+                </Link>
+                {selectedProject && selectedProject._id < data.length && (
+                    <Link href={`/${selectedProject._id + 1}`}>
+                        <Image
+                            src={arrow}
+                            alt="Arrow icon"
+                            width={50}
+                            height={25}
+                        />
+                        <p>Next project</p>
+                    </Link>
+                )}
+            </div>
         </section>
     );
 };
--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -18,39 +18,36 @@ const Gallery = () => {
     const x = useTransform(scrollYProgress, [0, 1], ["0.1%", "-85%"]);
 
     return (
-        <>
-            <section ref={targetRef} id={styles.gallery}>
-                {width < 800 ? (
-                    <>
-                        <h2>
-                            Some of my <span>work</span>
-                        </h2>
+        <section ref={targetRef} id={styles.gallery}>
+            {width < 800 ? (
+                <>
+                    <h2>
+                        Some of my <span>work</span>
+                    </h2>
+                    <Projects />
+                </>
+            ) : (
+                <div className={styles.horizontalContainer}>
+                    <motion.div style={{ x }}>
+                        <div className={styles.textWrapper}>
+                            <h2>
+                                Some of my <span>work</span>
+                            </h2>
+                            <figure>
+                                <Image
+                                    src={arrow}
+                                    alt="Arrow icon"
+                                    width={100}
+                                    height={80}
+                                />
+                                <figcaption>scroll to discover</figcaption>
+                            </figure>
+                        </div>
                         <Projects />
-                    </>
-                ) : (
-                    <div className={styles.horizontalContainer}>
-                        <motion.div style={{ x }}>
-                            <div className={styles.textWrapper}>
-                                <h2>
-                                    Some of my <span>work</span>
-                                </h2>
-                                <figure>
-                                    <Image
-                                        src={arrow}
-                                        alt="Arrow icon"
-                                        width={100}
-                                        height={80}
-                                    />
-                                    <figcaption>scroll to discover</figcaption>
-                                </figure>
-                            </div>
-                            <Projects />
-                        </motion.div>
-                    </div>
-                )}
-            </section>
-            <div style={{ height: "100vh" }}></div>
-        </>
+                    </motion.div>
+                </div>
+            )}
+        </section>
     );
 };
 
