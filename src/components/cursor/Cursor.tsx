ec6aba207a149fbeae2c0e148166899504b85673
3mpty <aurelien.corre@outlook.fr>
2024-03-12 19:06:19 +0100

    Added mouse trail effect

--- src/components/cursor/Cursor.tsx
+++ src/components/cursor/Cursor.tsx
@@ -1,36 +1,16 @@
-import React, { useEffect, useState } from "react";
-import styles from "./cursor.module.scss";
-import Image from "next/image";
-
-import cursor from "../../../public/icons/cursor.svg";
+import MouseTrail from "@pjsalita/react-mouse-trail";
 
 const Cursor = () => {
-    const [dotPosition, setDotPosition] = useState({ x: 0, y: 0 });
+    const config = {
+        color: "#000000",
+        idleAnimation: true,
+        idleAnimationCount: 10,
+        inverted: true,
+        size: 15,
+        trailCount: 50,
+    };
 
-    useEffect(() => {
-        const handleMouseMove = (e: any) => {
-            setDotPosition({ x: e.clientX - 15, y: e.clientY - 15 });
-        };
-
-        // Création des events
-        window.addEventListener("mousemove", handleMouseMove);
-
-        return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
-        };
-    }, []);
-
-    return (
-        <>
-            <div
-                className={styles.outline}
-                style={{
-                    transform: `translate(${dotPosition.x - 250}px, ${
-                        dotPosition.y - 250
-                    }px)`,
-                }}></div>
-        </>
-    );
+    return <MouseTrail {...config} />;
 };
 
 export default Cursor;
